bade2d77dd60148404053655ecea1dfa80fdd96d
Omar Zain <41789410+omarXzain@users.noreply.github.com>
2022-07-24 02:25:39 +0300

    Update sh.sh

--- Scripts/sh.sh
+++ Scripts/sh.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
- 
-echo "Enter Your Github UserName: "
-read x
-echo "Enter Your Github Email Address: "
-read y
-echo "Your Github USERNAME is ${x}!"
-echo "Your Email Address is ${y}"
-# viu ${y}
+
+read -p "Enter a word: " word
+echo "You entered $word"
