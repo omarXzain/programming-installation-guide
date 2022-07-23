8bca94e95d9a5293d2d683cde2ad6f918b2cf568
Omar Zain <41789410+omarXzain@users.noreply.github.com>
2022-07-24 02:28:37 +0300

    Update sh.sh

--- Scripts/sh.sh
+++ Scripts/sh.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#! /bin/bash
 
-read -p "Enter a word: " word
-echo "You entered $word"
+echo -n "Enter Fullname: "
+read fullname
+echo "your full name is $fullname."
+echo -n "test type more than 2 word: "
+read var1 var2; echo -e
+read var1 var2; echo -e "Var1: $var1 \nVar2: $var2")
