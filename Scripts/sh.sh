3492d153838afea98c66733b38969e997fb4b62a
Omar Zain <41789410+omarXzain@users.noreply.github.com>
2022-07-24 02:36:02 +0300

    Update sh.sh

--- Scripts/sh.sh
+++ Scripts/sh.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
-
-echo -n "Enter Fullname: "
-read fullname
-echo "your full name is $fullname."
-echo -n "test type more than 2 word: "
-read var1 var2; echo -e
-read var1 var2; echo -e "Var1: $var1 \nVar2: $var2")
+#/bin/sh
+printf 'Is this a good question (y/n)? '
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+    echo Yes
+else
+    echo No
+fi
